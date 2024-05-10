827cf0de0a100165251bd6dbc2ef3497d36fd0d0
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-10 09:16:49 -0600

    Update main.c

--- code/main.c
+++ code/main.c
@@ -16,6 +16,7 @@ Description:
   #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
 #endif
 
+
 int main(void) {
     /* Loop forever */
     for(;;);
