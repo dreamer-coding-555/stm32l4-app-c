3a0a2e2a0dc35e363a419deb19ebbb292864fef8
Michael Gene Brockus (Dreamer) <55331536+dreamer-coding-555@users.noreply.github.com>
2024-05-10 07:45:59 -0600

    Update main.c

--- code/main.c
+++ code/main.c
@@ -10,17 +10,13 @@ Description:
     feel free to contact Michael at michaelbrockus@gmail.com.
 ==============================================================================
 */
-extern int fossil_app_entry(int argc, char **argv);
+#include <stdint.h>
 
-/*
- > Main Function:
- > This is where program execution starts, and the application foundation function
- > is called, initiating the program's logic.
- >
- > @param argc The number of command-line arguments.
- > @param argv An array of strings containing the command-line arguments.
- > @return An integer indicating the application's exit status.
- */
-int main(int argc, char **argv) {
-    return fossil_app_entry(argc, argv);
-} // end of func
+#if !defined(__SOFT_FP__) && defined(__ARM_FP)
+  #warning "FPU is not initialized, but the project is compiling for an FPU. Please initialize the FPU before use."
+#endif
+
+int main(void) {
+    /* Loop forever */
+	for(;;);
+}
